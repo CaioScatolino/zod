3202164b8fdc8e529a3242d8742051d6aa02299b
Caio Scatolino <caio.scatolino@outlook.com>
2025-12-11 15:14:10 -0300

    feat: add Zod schema definition and validation example

--- src/zod.ts
+++ src/zod.ts
@@ -4,12 +4,32 @@ const schema = z.object({
   name: z.string().min(2),
   age: z.number().min(18).max(120),
   email: z.email(),
+  status: z.boolean().default(true),
+  characteristics: z.object({
+    strength: z.number().min(1).max(10),
+    agility: z.number().min(1).max(10),
+    intelligence: z.number().min(1).max(10),
+  }),
 });
 
-let data = schema.parse({
+type User = z.infer<typeof schema>;
+
+let data: User = {
   name: "John",
   age: 20,
   email: "john@example.com",
-});
+  status: true,
+  characteristics: {
+    strength: 10,
+    agility: 5,
+    intelligence: 4,
+  },
+};
 
-console.log(data);
+const result = schema.safeParse(data);
+
+if (result.success) {
+  console.log(result.data);
+} else {
+  console.log(result.error.issues);
+}
