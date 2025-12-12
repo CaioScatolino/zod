205dbe37a4f8310e75b408d637fd173b06e92e5b
Caio <caio.scatolino@outlook.com>
2025-12-12 20:13:12 -0300

    feat: expand `pattern` schema with new fields and advanced string, number, and date validations.

--- script.ts
+++ script.ts
@@ -1,11 +1,21 @@
 import z from "zod";
 
 const pattern = z.object({
-  name: z.literal("John"),
+  name: z.string().min(2).toUpperCase(),
+  age: z.number().min(18).max(120),
+  email: z.email().toLowerCase(),
+  description: z.string().trim(),
+  date: z.date(),
+  imagem: z.string().endsWith(".png"),
 });
 
 const result = pattern.parse({
   name: "John",
+  age: 20,
+  email: "JOHN@example.com",
+  description: "     a qwe  q      ",
+  date: new Date("2025-12-12"),
+  imagem: "wp_123.png",
 });
 
 console.log(result);
